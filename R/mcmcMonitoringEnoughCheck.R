374eade7f65078d5db6fefe3e40784061c2acf93
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-02-15 06:20:39 -0600

    monitoringEnoughCheck - added for more wait time round 2 (had infinite loop)

--- R/mcmcMonitoringEnoughCheck.R
+++ R/mcmcMonitoringEnoughCheck.R
@@ -8,12 +8,17 @@
 
 mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW){
 
+  obs <- nrow(o)
+
   # Check that sufficient n for burn in CI width
   minN <- ceiling((2 * 1.96 / minWW)^2)
+  if(nrow(obs) - minN < 0) {
+    waitMoreN <- minN - nrow(obs)
+  }
 
   # Check for stability of sgpv
-  stabilityNonTrivial <- sum(o[(nrow(o)-maxAlertSteps + 1):nrow(o),"sgpvNonTrivial"]==1)
-  stabilityFutility   <- sum(o[(nrow(o)-maxAlertSteps + 1):nrow(o),"sgpvFutility"]==1)
+  stabilityNonTrivial <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvNonTrivial"]==1)
+  stabilityFutility   <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvFutility"]==1)
 
   # Return T/F regarding stability
   # Get a sense of how many additional observations needed and multiple by arbitraty factor of 4
