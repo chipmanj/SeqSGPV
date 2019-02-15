c04b58a9718bedbca2730b21f97964ccd0e44a5d
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-02-15 05:44:15 -0600

    added to check making sure enough to estimate the smallest wait width

--- R/mcmcMonitoringEnoughCheck.R
+++ R/mcmcMonitoringEnoughCheck.R
@@ -6,7 +6,10 @@
 
 
 
-mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps){
+mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW){
+
+  # Check that sufficient n for burn in CI width
+  minN <- ceiling((2 * 1.96 / minWW)^2)
 
   # Check for stability of sgpv
   stabilityNonTrivial <- sum(o[(nrow(o)-maxAlertSteps + 1):nrow(o),"sgpvNonTrivial"]==1)
@@ -14,7 +17,8 @@ mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps){
 
   # Return T/F regarding stability
   # Get a sense of how many additional observations needed and multiple by arbitraty factor of 4
-  getMore <- min(maxAlertSteps - stabilityNonTrivial, maxAlertSteps - stabilityFutility) * 4
+  getMore0 <- min(maxAlertSteps - stabilityNonTrivial, maxAlertSteps - stabilityFutility) * 4
+  getMore  <- min(getMore0, minN)
 
   getMore
 }
