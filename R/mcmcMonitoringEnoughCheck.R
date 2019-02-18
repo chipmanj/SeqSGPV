72b0575e5d645ad54d2a4c5f81efa5c21897b3d9
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-02-18 09:31:18 -0600

    If waitTimeN > n needed for stability, use waitTimeN.  Else use n for more stability.

--- R/mcmcMonitoringEnoughCheck.R
+++ R/mcmcMonitoringEnoughCheck.R
@@ -14,16 +14,23 @@ mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW){
   minN <- ceiling((2 * 1.96 / minWW)^2)
   if(obs - minN <= 0) {
          waitMoreN <- minN - obs
-  } else waitMoreN <- NA
+  } else waitMoreN <- 0
 
   # Check for stability of sgpv
   stabilityNonTrivial <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvNonTrivial"]==1)
   stabilityFutility   <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvFutility"]==1)
 
-  # Return T/F regarding stability
   # Get a sense of how many additional observations needed and multiple by arbitraty factor of 4
   getMore0 <- min(maxAlertSteps - stabilityNonTrivial, maxAlertSteps - stabilityFutility) * 5
-  getMore  <- min(getMore0, waitMoreN, na.rm = TRUE)
+
+
+  # If required burn in time is greater than n to achive stability, use burn in time (burning in too long)
+  # Else use n required to achieve greater stability
+  if(waitMoreN > getMore0){
+    getMore <- waitMoreN
+  } else {
+    getMore <- getMore0
+  }
 
   getMore
 }
