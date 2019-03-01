04e432fe82a2a6d3200865fe7d2f494ff5977d1a
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-03-01 09:08:37 -0600

    removed delta inputs from sgpvAMrules.  Done bc mcmcEnoughCheck occurs before applying rules and requires sgpv.  New function previously add sgpv to data.

--- R/sgpvAMrules.R
+++ R/sgpvAMrules.R
@@ -9,10 +9,6 @@
 #
 # Data:                    matrix from sgpvAMdata.  Or a similar matrix with columns:
 #                          n, y, lo, hi, z
-# deltaL1:                 The delta less than and closest to the point null.
-# deltaL2:                 The delta less than and furthest from the point null.
-# deltaG1:                 The delta greater than and closest to the point null.
-# deltaG2:                 The delta greater than and furthest from the point null.
 # waitWidth:               The width of the confidence interval under an assumed variance
 # monitoringIntervalLevel: The traditional alpha
 # lookSteps:               How frequently will data be monitored after burn in
@@ -20,35 +16,8 @@
 
 
 
-sgpvAMrules <- function(data,
-                        deltaL2, deltaL1, deltaG1, deltaG2,
-                        waitWidth, monitoringIntervalLevel, lookSteps, maxAlertSteps=100){
+sgpvAMrules <- function(data, waitWidth, monitoringIntervalLevel, lookSteps, maxAlertSteps=100){
 
-
-
-
-  # 1 Obtain sgpv
-  if(!anyNA(c(deltaL2, deltaL1, deltaG1, deltaG2))){
-
-    # Two sided
-    sgpvNonTrivial <- 1 - sgpv::sgpvalue(est.lo = data[,"lo"], est.hi = data[,"up"], null.lo = deltaL1, null.hi = deltaG1)$p.delta
-    sgpvFutility   <-     sgpv::sgpvalue(est.lo = data[,"lo"], est.hi = data[,"up"], null.lo = deltaL2, null.hi = deltaG2)$p.delta
-  } else if(!anyNA(c(deltaL2, deltaL1))){
-
-    # One sided: efficacy when less than null
-    sgpvNonTrivial <- 1 - sgpv::sgpvalue(est.lo = data[,"lo"], est.hi = data[,"up"], null.lo =  -10^10, null.hi = deltaL1)$p.delta
-    sgpvFutility   <-     sgpv::sgpvalue(est.lo = data[,"lo"], est.hi = data[,"up"], null.lo = deltaL2, null.hi = 10^10)$p.delta
-  } else if(!anyNA(c(deltaG1, deltaG2))){
-
-    # One sided: efficacy when less than null
-    sgpvNonTrivial <- 1 - sgpv::sgpvalue(est.lo = data[,"lo"], est.hi = data[,"up"], null.lo = deltaG1, null.hi = 10^10)$p.delta
-    sgpvFutility   <-     sgpv::sgpvalue(est.lo = data[,"lo"], est.hi = data[,"up"], null.lo =  -10^10, null.hi = deltaG2)$p.delta
-  } else{
-
-    stop("A one sided study requires both deltas to be strictly greater or lower than point null")
-  }
-
-  data <- cbind(data,sgpvNonTrivial,sgpvFutility)
 
 
 
@@ -60,8 +29,8 @@ sgpvAMrules <- function(data,
 
 
   # 3 Raise Alert
-  alertNonTrivial <- which(sgpvNonTrivial == 1 & monitor==TRUE)
-  alertFutility   <- which(sgpvFutility   == 1 & monitor==TRUE)
+  alertNonTrivial <- which(data[,"sgpvNonTrivial"] == 1 & monitor==TRUE)
+  alertFutility   <- which(data[,"sgpvFutility"]   == 1 & monitor==TRUE)
 
   alertNonTrivialAny <- length(alertNonTrivial)>0
   alertFutilityAny   <- length(alertFutility)  >0
