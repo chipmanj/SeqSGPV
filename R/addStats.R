276f06aca4b07ceb647d2f0d3e58d56ca7be8632
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-11 14:43:19 -0500

    addStats function to append rejectPN, bias, cover, and sgpv to sequentially monitored data

---
+++ R/addStats.R
@@ -0,0 +1,62 @@
+# addStats
+#
+# adds statistics following each new observation
+# - reject point null
+# - bias
+# - coverage
+# - sgpv Non Trivial and Futility
+#
+# Inputs
+# o        : matrix with at least columns: theta, est, lo (lower CI bound), and up (upper CI bound)
+# pointNull: specified point null
+# deltaL2  : lower delta furthest from point null
+# deltaL1  : lower delta closest    to point null
+# deltaG1  : upper delta furthest from point null
+# deltaG2  : upper delta closest    to point null
+#
+# Returns
+# o with new columns appended
+# - rejectPN (reject point null)
+# - bias
+# - cover (does CI cover theta)
+# - sgpvNonTrivial
+# - sgpvFutility
+
+
+
+addStats <- function(o, pointNull, deltaL2, deltaL1, deltaG1, deltaG2){
+
+  # Add whether reject point null, coverage, and bias
+  rejPN <- as.numeric(o[,"lo"] < pointNull & o[,"up"] < pointNull |
+                      o[,"lo"] > pointNull & o[,"up"] > pointNull)
+  cover <- as.numeric(o[,"lo"] < theta & theta < o[,"up"])
+  bias  <- o[,"est"] - theta
+
+
+  # Obtain sgpv
+  if(!anyNA(c(deltaL2, deltaL1, deltaG1, deltaG2))){
+
+    # Two sided
+    sgpvNonTrivial <- 1 - sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaL1, null.hi = deltaG1)$p.delta
+    sgpvFutility   <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaL2, null.hi = deltaG2)$p.delta
+  } else if(!anyNA(c(deltaL2, deltaL1))){
+
+    # One sided: efficacy when less than null
+    sgpvNonTrivial <- 1 - sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =  -10^10, null.hi = deltaL1)$p.delta
+    sgpvFutility   <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaL2, null.hi = 10^10)$p.delta
+  } else if(!anyNA(c(deltaG1, deltaG2))){
+
+    # One sided: efficacy when less than null
+    sgpvNonTrivial <- 1 - sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaG1, null.hi = 10^10)$p.delta
+    sgpvFutility   <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =  -10^10, null.hi = deltaG2)$p.delta
+  } else{
+
+    stop("A one sided study requires both deltas to be strictly greater or lower than point null")
+  }
+
+
+  # Return appended statistics
+  cbind(o,bias,rejPN,cover, sgpvNonTrivial, sgpvFutility)
+
+}
+
