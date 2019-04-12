c55e74a96738181a70669bad4c3a6e00fbf3aa8d
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-12 11:50:36 -0500

    comment out example

--- R/sgpvAM.R
+++ R/sgpvAM.R
@@ -122,13 +122,13 @@ sgpvAM <- function(mcmcData=NULL, nreps, maxAlertSteps=100, lookSteps=1,
 # Examples
 
 # No previously generated data
-am1 <- sgpvAM(nreps = 100,
-              maxAlertSteps = 100, lookSteps = 1, waitWidths = seq(0.15, 0.6, by = 0.05),
-              dataGeneration = rnorm,   dataGenArgs = list(n=800),
-              effectGeneration = 0,
-              modelFit = lmCI,
-              pointNull = 0, deltaL2 = -0.4, deltaL1=-0.3, deltaG1=0.3, deltaG2=0.4,
-              monitoringIntervalLevel=0.05)
+# am1 <- sgpvAM(nreps = 100,
+#               maxAlertSteps = 100, lookSteps = 1, waitWidths = seq(0.15, 0.6, by = 0.05),
+#               dataGeneration = rnorm,   dataGenArgs = list(n=800),
+#               effectGeneration = 0,
+#               modelFit = lmCI,
+#               pointNull = 0, deltaL2 = -0.4, deltaL1=-0.3, deltaG1=0.3, deltaG2=0.4,
+#               monitoringIntervalLevel=0.05)
 
 
 
