395d2a9cbd7a2ea7c6c314fd3432db51a1222130
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-15 18:00:55 -0500

    sets minN based on provided sd (ie not assumed to be standardized to one), changed stability objects to Trivial and Impactful from NonTrivial and Futility.

--- R/mcmcMonitoringEnoughCheck.R
+++ R/mcmcMonitoringEnoughCheck.R
@@ -6,12 +6,12 @@
 
 
 
-mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW){
+mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW, sd){
 
   obs <- nrow(o)
 
   # Check that sufficient n for burn in CI width
-  minN <- ceiling((2 * 1.96 / minWW)^2)
+  minN <- ceiling((2 * 1.96 * sd / minWW)^2)
   if(obs - minN <= 0) {
          waitMoreN <- minN - obs + maxAlertSteps
   } else waitMoreN <- 0
@@ -19,11 +19,11 @@ mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW){
   # Check for stability of sgpv
   #  - Require at least as many observations as maxAlertSteps
   if(obs > maxAlertSteps){
-    stabilityNonTrivial <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvNonTrivial"]==1)
-    stabilityFutility   <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvFutility"]==1)
+    stabilityTrivial   <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvTrivial"]==0)
+    stabilityImpactful <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvImpactful"]==0)
 
     # Get a sense of how many additional observations needed and multiple by arbitraty factor of 4
-    getMore0 <- min(maxAlertSteps - stabilityNonTrivial, maxAlertSteps - stabilityFutility) * 5
+    getMore0 <- min(maxAlertSteps - stabilityTrivial, maxAlertSteps - stabilityImpactful) * 5
   } else {
     getMore0 <- maxAlertSteps * 5
   }
@@ -40,3 +40,20 @@ mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW){
   getMore
 }
 
+
+pooledVariance <- function(i){
+  dd  <- d[1:i,]
+  y0  <- dd[dd[,"trt"]==0,"y"]
+  y1  <- dd[dd[,"trt"]==1,"y"]
+  vp <- ((length(y0) - 1) * var(y0) + (length(y1) - 1) * var(y1)) / (i - 2)
+  vp
+}
+
+
+plot(x=4:nrow(d),y=sapply(4:nrow(d),pooledVariance),type="l",xlim=c(0,200))
+abline(v=15,col="red")
+abline(v=30,col="red")
+
+var(sapply(4:14,pooledVariance))
+var(sapply(15:24,pooledVariance))
+var(sapply(25:34,pooledVariance))
