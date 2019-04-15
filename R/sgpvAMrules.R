f8ed681d62c279beea0383f6c946395229e6d229
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-15 18:05:16 -0500

    1) new rules for maxN and lagOutcome 2) new names for alertNotTrivial and alertNotImpactful

--- R/sgpvAMrules.R
+++ R/sgpvAMrules.R
@@ -8,7 +8,7 @@
 #  maxAlertSteps by how frequently data are monitored.
 #
 # Data: matrix from sgpvAMdata.  Or a similar matrix with columns:
-#       n, y, lo, hi, sgpvNonTrivial, sgpvFutility, z
+#       theta, n, y, lo, hi, sgpvTrivial, sgpvImpactful,
 # waitWidth:               The width of the confidence interval under an assumed variance
 # monitoringIntervalLevel: The traditional alpha
 # lookSteps:               How frequently will data be monitored after burn in
@@ -16,8 +16,10 @@
 
 
 
-sgpvAMrules <- function(data, waitWidth, monitoringIntervalLevel, lookSteps, maxAlertSteps=100){
-
+sgpvAMrules <- function(data,      waitWidth,
+                        monitoringIntervalLevel,
+                        lookSteps, maxAlertSteps=100,
+                        maxN,      lagOutcomeN){
 
   # 1 Establish observations that surpass wait time and occur at lookSteps
   waitTime  <- ceiling((2 * qnorm(1 - monitoringIntervalLevel / 2) * 1 / waitWidth)^2)
@@ -27,35 +29,83 @@ sgpvAMrules <- function(data, waitWidth, monitoringIntervalLevel, lookSteps, max
 
 
   # 2 Raise Alert
-  alertNonTrivial <- which(data[,"sgpvNonTrivial"] == 1 & monitor==TRUE)
-  alertFutility   <- which(data[,"sgpvFutility"]   == 1 & monitor==TRUE)
+  alertNotTrivial   <- which(data[,"sgpvTrivial"]   == 0 & monitor==TRUE)
+  alertNotImpactful <- which(data[,"sgpvImpactful"] == 0 & monitor==TRUE)
 
-  alertNonTrivialAny <- length(alertNonTrivial)>0
-  alertFutilityAny   <- length(alertFutility)  >0
+  alertNotTrivialAny   <- length(alertNotTrivial)   > 0
+  alertNotImpactfulAny <- length(alertNotImpactful) > 0
 
 
 
-  # 3 Affirm alert and report end of study (eos)
+  # 3 Affirm alert and report end of study (eos) operating characteristics (oc)
+  #   - To be applied across various k (affirmation steps)
   affirmEndOfStudy <- function(alertK){
 
-    if(alertNonTrivialAny) {
-      sNT    <- alertNonTrivial[alertNonTrivial %in% (alertNonTrivial + alertK)]
+
+    if(alertNotTrivialAny) {
+      sNT    <- alertNotTrivial[alertNotTrivial %in% (alertNotTrivial + alertK)]
       if(length(sNT) > 0) {
-             stopNonTrivial <- min(sNT)
-      } else stopNonTrivial <- NA
-    } else   stopNonTrivial <- NA
+             stopNotTrivial <- min(sNT)
+      } else stopNotTrivial <- NA
+    } else   stopNotTrivial <- NA
 
-    if(alertFutilityAny) {
-      sF     <- alertFutility[alertFutility %in% (alertFutility + alertK)]
-      if(length(sF) > 0) {
-             stopFutility <- min(sF)
-      } else stopFutility <- NA
-    } else   stopFutility <- NA
+    if(alertNotImpactfulAny) {
+      sNI     <- alertNotImpactful[alertNotImpactful %in% (alertNotImpactful + alertK)]
+      if(length(sNI) > 0) {
+             stopNotImpactful <- min(sNI)
+      } else stopNotImpactful <- NA
+    } else   stopNotImpactful <- NA
 
-    stop   <- min(stopNonTrivial,stopFutility,na.rm = TRUE)
-    eos    <- data[data[,"n"]==stop,]
+
+
+    # Unconstrained N stop
+    stop                     <- min(stopNotTrivial, stopNotImpactful, na.rm = TRUE)
+    eos                      <- data[data[,"n"]==stop,]
+    eos["stopNotTrivial"]    <- as.numeric(eos["sgpvTrivial"]==0)
+    eos["stopNotImpactful"]  <- as.numeric(eos["sgpvImpactful"]==0)
+    eos["stopInconclusive"]  <- as.numeric(eos["stopNotTrivial"]==0 & eos["stopNotImpactful"]==0)
+
+    # Unconstrained with lag on outcome
+    if(!is.null(lagOutcomeN)){
+      eosLag                     <- data[data[,"n"]==stop + lagOutcomeN,]
+      eosLag["stopNotTrivial"]   <- as.numeric(eosLag["sgpvTrivial"]==0)
+      eosLag["stopNotImpactful"] <- as.numeric(eosLag["sgpvImpactful"]==0)
+      eosLag["stopInconclusive"] <- as.numeric(eosLag["stopNotTrivial"]==0 &
+                                               eosLag["stopNotImpactful"]==0)
+    } else eosLag <- NULL
+
+    # MaxN stop
+    if(!is.null(maxN)){
+      eosMaxN                     <- data[data[,"n"]==min(stop,maxN),]
+      eosMaxN["stopNotTrivial"]   <- as.numeric(eosMaxN["sgpvTrivial"]==0)
+      eosMaxN["stopNotImpactful"] <- as.numeric(eosMaxN["sgpvImpactful"]==0)
+      eosMaxN["stopInconclusive"] <- as.numeric(eosMaxN["stopNotTrivial"]==0 &
+                                                eosMaxN["stopNotImpactful"]==0)
+    } else eosMaxN <- NULL
+
+    # lag on outcome, having enrolled MaxN stop
+    if(!is.null(lagOutcomeN) & !is.null(maxN)){
+      eosLagMaxN                     <- data[data[,"n"]==min(stop,maxN)+lagOutcomeN,]
+      eosLagMaxN["stopNotTrivial"]   <- as.numeric(eosLagMaxN["sgpvTrivial"]==0)
+      eosLagMaxN["stopNotImpactful"] <- as.numeric(eosLagMaxN["sgpvImpactful"]==0)
+      eosLagMaxN["stopInconclusive"] <- as.numeric(eosLagMaxN["stopNotTrivial"]==0 &
+                                                   eosLagMaxN["stopNotImpactful"]==0)
+    } else eosLagMaxN <- NULL
+
+    # Keep theta and operating characteristics (drop y, trt, and sgpvs)
+    keepStats <- c("n","est","bias","rejPN","cover", "stopNotTrivial","stopNotImpactful","stopInconclusive")
+
+
     if(!is.na(stop)){
-      return(c(eos,alertK=alertK))
+      oc <- c(eos[c("theta", keepStats)],
+              lag     = eosLag[keepStats],
+              maxN    = eosMaxN[keepStats],
+              lagMaxN = eosLagMaxN[keepStats])
+      # oc <- c(eos[c("theta", keepStats)],
+      #         lag     = eosLag[keepStats],
+      #         maxN    = eosMaxN[keepStats])
+
+      return(c(oc,alertK=alertK))
     } else {
       return(NULL)
     }
