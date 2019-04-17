1c78ef6ca3a7a4f35370c78473799f12ab0c1a00
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-17 17:15:38 -0500

    updated example

--- R/example.R
+++ R/example.R
@@ -1,51 +1,54 @@
-# devtools::install_github("chipmanj/sgpvAM")
-# library(sgpvAM)
-#
+# # devtools::install_github("chipmanj/sgpvAM")
+# # library(sgpvAM)
+# #
 # # Simulate AM trial
 # # Two-sided deltas
 # # defaults to sd = 1
-# am <-  sgpvAM(nreps            = 100,
-#               maxAlertSteps    = 100,       lookSteps = 1, waitWidths = seq(0.15, 0.6, by = 0.05),
-#               dataGeneration   = rnorm,   dataGenArgs = list(n=800),
-#               effectGeneration = 0,
-#               modelFit         = lmCI,
-#               pointNull = 0, deltaL2 = -0.5, deltaL1=-0.2, deltaG1=0.2, deltaG2=0.5,
-#               monitoringIntervalLevel = 0.05,
-#               maxN = 200, lagOutcomeN = 50)
+# system.time(am <-  sgpvAM(nreps            = 1000,
+#                           maxAlertSteps    = 100,       lookSteps = 1, kSteps = 10,
+#                           waitWidths       = seq(0.15, 0.6, by = 0.05),
+#                           dataGeneration   = rnorm,   dataGenArgs = list(n=200),
+#                           effectGeneration = 0,
+#                           modelFit         = lmCI,
+#                           pointNull = 0, deltaL2 = -0.5, deltaL1=-0.2, deltaG1=0.2, deltaG2=0.5,
+#                           monitoringIntervalLevel = 0.05,
+#                           printProgress = TRUE,
+#                           maxN = 200, lagOutcomeN = 50))
 #
-# amShifted <- locationShift(am, shiftedThetas = seq(-1,0,by=0.1))
+# system.time(amShifted <- locationShift(am, shiftedThetas = c(seq(-0.6,-0.1,by=0.05))))
 #
 #
 # # Unrestricted Sample Size
 # # Explore wait width (first with alert k specified at 50)
-# plot(amShifted, "n",                 alertK = 50,      xlim=c(-1,0),   ylim=c(0,801),    maxVary = 10)
-# plot(amShifted, "rejPN",             alertK = 50,      xlim=c(-1,0),   ylim=c(0,1),      maxVary = 10)
-# plot(amShifted, "rejPN",             alertK = 50,      xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
-# plot(amShifted, "bias",              alertK = 50,      xlim=c(-1,0),   ylim=c(-0.1,0.1), maxVary = 5)
-# plot(amShifted, "mse",               alertK = 50,      xlim=c(-1,0),   ylim=c(0,0.1),    maxVary = 5)
-# plot(amShifted, "cover",             alertK = 50,      xlim=c(-1,0),   ylim=c(0.75,1),   maxVary = 5)
-# plot(amShifted, "stopInconclusive",  alertK = 50,      xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
-# plot(amShifted, "stopNotImpactful",  alertK = 50,      xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
-# plot(amShifted, "stopNotTrivial",    alertK = 50,      xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "n",                 alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,801),    maxVary = 10)
+# plot(amShifted, "n",                 alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,801),    maxVary = 5)
+# plot(amShifted, "rejPN",             alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 10)
+# plot(amShifted, "rejPN",             alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "bias",              alertK = 50,      xlim=c(-1,0.4),   ylim=c(-0.1,0.1), maxVary = 5)
+# plot(amShifted, "mse",               alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,0.1),    maxVary = 5)
+# plot(amShifted, "cover",             alertK = 50,      xlim=c(-1,0.4),   ylim=c(0.75,1),   maxVary = 5)
+# plot(amShifted, "stopInconclusive",  alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "stopNotImpactful",  alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "stopNotTrivial",    alertK = 50,      xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
 #
 # # Explore number of steps in affirmation step
-# plot(amShifted, "n",                 waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(0,801),    maxVary = 5)
-# plot(amShifted, "bias",              waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(-0.1,0.1), maxVary = 5)
-# plot(amShifted, "mse",               waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(0,0.1),    maxVary = 5)
-# plot(amShifted, "cover",             waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(0.75,1),   maxVary = 5)
-# plot(amShifted, "stopInconclusive",  waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
-# plot(amShifted, "stopNotImpactful",  waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
-# plot(amShifted, "stopNotTrivial",    waitWidth = 0.3,  xlim=c(-1,0),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "n",                 waitWidth = 0.35,  xlim=c(-1,0.4),   ylim=c(0,801),    maxVary = 5)
+# plot(amShifted, "bias",              waitWidth = 0.3,  xlim=c(-1,0.4),   ylim=c(-0.1,0.1), maxVary = 5)
+# plot(amShifted, "mse",               waitWidth = 0.3,  xlim=c(-1,0.4),   ylim=c(0,0.1),    maxVary = 5)
+# plot(amShifted, "cover",             waitWidth = 0.3,  xlim=c(-1,0.4),   ylim=c(0.75,1),   maxVary = 5)
+# plot(amShifted, "stopInconclusive",  waitWidth = 0.3,  xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "stopNotImpactful",  waitWidth = 0.3,  xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
+# plot(amShifted, "stopNotTrivial",    waitWidth = 0.3,  xlim=c(-1,0.4),   ylim=c(0,1),      maxVary = 5)
 #
 #
 # # Unrestricted Sample Size with lag of remaining outcomes
-# plot(amShifted, "n",                 alertK = 50,     xlim=c(-1,0),   ylim=c(0,801), maxVary = 10, sizeRestrictions = "lag")
-# plot(amShifted, "stopInconclusive",  alertK = 50,     xlim=c(-1,0),   ylim=c(0,1),   maxVary = 10, sizeRestrictions = "lag")
-# plot(amShifted, "stopInconclusive",  waitWidth = 0.3, xlim=c(-1,0),   ylim=c(0,1),   maxVary = 10, sizeRestrictions = "lag")
+# plot(amShifted, "n",                 alertK = 50,     xlim=c(-1,0.4),   ylim=c(0,801), maxVary = 10, sizeRestrictions = "lag")
+# plot(amShifted, "stopInconclusive",  alertK = 50,     xlim=c(-1,0.4),   ylim=c(0,1),   maxVary = 10, sizeRestrictions = "lag")
+# plot(amShifted, "stopInconclusive",  waitWidth = 0.3, xlim=c(-1,0.4),   ylim=c(0,1),   maxVary = 10, sizeRestrictions = "lag")
 #
 # # Max N with immediate outcomes
 # plot(amShifted, "n",     alertK = 50,     xlim=c(-1,0),   ylim=c(0,801), maxVary = 10, sizeRestrictions = "maxN")
-# plot(amShifted, "stopInconclusive",     alertK = 50,     xlim=c(-1,0),   ylim=c(0,1), maxVary = 10, sizeRestrictions = "lag")
+# plot(amShifted, "stopInconclusive",     alertK = 50,     xlim=c(-1,0.4),   ylim=c(0,1), maxVary = 10, sizeRestrictions = "lag")
 #
 #
 # summary(amShifted, alertK = 50, waitTime = 0.3, treatEffect = -0.5)
@@ -59,16 +62,16 @@
 # # Simulate AM trial
 # # One-sided deltas
 # # Outcome has sd = 2
-# am2 <-  sgpvAM(nreps           = 100,
+# system.time(am2 <-  sgpvAM(nreps           = 1000,
 #               maxAlertSteps    = 100,       lookSteps = 1, waitWidths = sort(c(seq(0.2, 1.2, by = 0.2),c(0.5,0.7))),
-#               dataGeneration   = rnorm,   dataGenArgs = list(n=800, sd=2),
+#               dataGeneration   = rnorm,   dataGenArgs = list(n=200, sd=2),
 #               effectGeneration = 0,
 #               modelFit         = lmCI,
 #               pointNull = 0, deltaL2 = -1, deltaL1=-0.2, deltaG1=NA, deltaG2=NA,
 #               monitoringIntervalLevel = 0.05,
-#               maxN = 200, lagOutcomeN = 50)
+#               maxN = 200, lagOutcomeN = 50))
 #
-# amShifted2 <- locationShift(am2, shiftedThetas = seq(-2,0.8,by=0.4))
+# system.time(amShifted2 <- locationShift(am2, shiftedThetas = seq(-2,0.8,by=0.4)))
 #
 #
 #
