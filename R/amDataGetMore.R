6c84b9c7721c0a53d55f4a6f0f6e34fef7bf866c
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-17 17:11:29 -0500

    parallelized getting more data with amDataGetMore

---
+++ R/amDataGetMore.R
@@ -0,0 +1,50 @@
+#' amDataGetMore
+#'
+#' Generate mcmc simulations of adaptive monitoring with parallel computing
+#'
+#' @export
+amDataGetMore <- function(insufficients, existingDataList, fork=TRUE, socket = TRUE, cores = detectCores(), ...){
+
+  if(fork==TRUE){
+    # Only works on POSIX systems (Mac, Linux, Unix, BSD) and not Windows.
+    mcmcMonitoringGetMore <- parallel::mclapply(insufficients,     amDataSingleGetMore,
+                                                existingDataList = existingDataList,
+                                                ... ,   mc.cores = cores)
+  } else if(socket==TRUE){
+    # Works on Mac and Windows; only slightly slower
+    cl             <- parallel::makeCluster(cores)
+    on.exit(parallel::stopCluster(cl), add = TRUE)
+    mcmcMonitoringGetMore <- parallel::parLapply(cl, insufficients, amDataSingleGetMore,
+                                                 existingDataList = existingDataList, ...)
+  } else {
+
+    mcmcMonitoringGetMore <- lapply(getMoreWhich, amDataSingleGetMore,
+                                    existingDataList = mcmcMonitoring, getMore = getMore,
+                                    monitoringIntervalLevel = monitoringIntervalLevel,
+                                    dataGeneration   = dataGeneration,   dataGenArgs   = dataGenArgs,
+                                    effectGeneration = effectGeneration, effectGenArgs = effectGenArgs,
+                                    modelFit         = modelFit)
+  }
+
+
+  # update existing data
+  names(existingDataList)      <- 1:length(existingDataList)
+  names(mcmcMonitoringGetMore) <- insufficients
+  mcmcMonitoring               <- modifyList(existingDataList, mcmcMonitoringGetMore, keep.null = TRUE)
+
+
+  # Return updated
+  return(mcmcMonitoring)
+}
+
+# test3 <- amDataGetMore(insufficients = getMoreWhich, existingDataList = mcmcMonitoring, getMore = getMore,
+#               monitoringIntervalLevel = monitoringIntervalLevel,
+#               dataGeneration   = dataGeneration,   dataGenArgs   = dataGenArgs,
+#               effectGeneration = effectGeneration, effectGenArgs = effectGenArgs,
+#               modelFit         = modelFit, fork=FALSE,socket=FALSE)
+#
+# test <- lapply(getMoreWhich, amDataGetMore, existingDataList = mcmcMonitoring, getMore = getMore,
+#                monitoringIntervalLevel = monitoringIntervalLevel,
+#                dataGeneration   = dataGeneration,   dataGenArgs   = dataGenArgs,
+#                effectGeneration = effectGeneration, effectGenArgs = effectGenArgs,
+#                modelFit         = modelFit)
---
+++ R/amDataSingleGetMore.R
@@ -0,0 +1,39 @@
+#' amDataSingleGetMore
+#'
+#' Generate additional observations for single mcmc simulation
+#'
+#' @export
+amDataSingleGetMore <- function( existingDataList, iInsufficient, getMore,
+                                 monitoringIntervalLevel,
+                                 dataGeneration,   dataGenArgs,
+                                 effectGeneration, effectGenArgs,
+                                 modelFit ){
+
+  dataGenArgs$n     <- getMore[iInsufficient]
+
+  amDataSingleMore  <- amDataSingle( monitoringIntervalLevel = monitoringIntervalLevel,
+                                     existingData     = existingDataList[[iInsufficient]],
+                                     dataGeneration   = dataGeneration,   dataGenArgs   = dataGenArgs,
+                                     effectGeneration = effectGeneration, effectGenArgs = effectGenArgs,
+                                     modelFit         = modelFit)
+
+  amDataSingleMore  <- addStats(o = amDataSingleMore,
+                                pointNull = pointNull,
+                                deltaL2   = deltaL2, deltaL1 = deltaL1,
+                                deltaG1   = deltaG1, deltaG2 = deltaG2)
+
+  return(amDataSingleMore)
+}
+
+
+# test <- amDataSingleGetMore(mcmcMonitoring, getMoreWhich[1], getMore)
+# test <- lapply(getMoreWhich, amDataSingleGetMore, existingDataList = mcmcMonitoring, getMore = getMore,
+#                monitoringIntervalLevel = monitoringIntervalLevel,
+#                dataGeneration   = dataGeneration,   dataGenArgs   = dataGenArgs,
+#                effectGeneration = effectGeneration, effectGenArgs = effectGenArgs,
+#                modelFit         = modelFit)
+#
+# names(mcmcMonitoring) <- 1:nreps
+# names(test) <- getMoreWhich
+#
+# test2 <- modifyList(mcmcMonitoring,test,keep.null = TRUE)
