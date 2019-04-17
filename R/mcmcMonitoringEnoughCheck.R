20f19ae4cfedebc1674fce2bf99d3f0b8a8164f4
Jonathan Chipman <jonathan.chipman@vanderbilt.edu>
2019-04-17 17:14:01 -0500

    monitoringEnoughChecks, makes sure has enough for maxAlertSteps AND lagOutcomeN

--- R/mcmcMonitoringEnoughCheck.R
+++ R/mcmcMonitoringEnoughCheck.R
@@ -8,26 +8,29 @@
 
 
 
-mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW, sd){
+mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, lagOutcomeN, minWW, sd){
 
   obs <- nrow(o)
 
-  # Check that sufficient n for burn in CI width
-  minN <- ceiling((2 * 1.96 * sd / minWW)^2)
+  # Check that sufficient observed n for burn in CI width + the lag time on outcome for unobserved outcomes
+  waitN <- ceiling((2 * 1.96 * sd / minWW)^2)
+  minN  <- waitN + maxAlertSteps + lagOutcomeN
   if(obs - minN <= 0) {
          waitMoreN <- minN - obs + maxAlertSteps
   } else waitMoreN <- 0
 
   # Check for stability of sgpv
   #  - Require at least as many observations as maxAlertSteps
-  if(obs > maxAlertSteps){
-    stabilityTrivial   <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvTrivial"]==0)
-    stabilityImpactful <- sum(o[(obs-maxAlertSteps + 1):obs,"sgpvImpactful"]==0)
+  stabilityN <- maxAlertSteps + lagOutcomeN
+  if(obs > stabilityN ){
+    stabilityTrivial   <- sum(o[(obs-stabilityN + 1):obs,"sgpvTrivial"]==0)
+    stabilityImpactful <- sum(o[(obs-stabilityN + 1):obs,"sgpvImpactful"]==0)
 
     # Get a sense of how many additional observations needed and multiple by arbitraty factor of 4
-    getMore0 <- min(maxAlertSteps - stabilityTrivial, maxAlertSteps - stabilityImpactful) * 5
+    getMore0 <- min(stabilityN - stabilityTrivial, stabilityN - stabilityImpactful) * 4
+
   } else {
-    getMore0 <- maxAlertSteps * 5
+    getMore0 <- stabilityN * 4
   }
 
 
@@ -38,6 +41,9 @@ mcmcMonitoringEnoughCheck <- function(o, maxAlertSteps, minWW, sd){
   } else {
     getMore <- getMore0
   }
+
+  # If getting more, get at least 50 more observations (arbitrary)
+  if(getMore > 0 & getMore < 50) getMore <- max(getMore, 50)
 
   getMore
 }
