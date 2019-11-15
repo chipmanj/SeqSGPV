5da7a57aa65d4c76642f26de5023a0cae6ecd071
Jonathan Chipman <jonathanchipman@jonathans-mini.hci.utah.edu>
2019-11-15 16:08:45 -0700

    Change from waitWidth to waitTime, add input for getUnrestricted and only collect more data when TRUE.

--- R/sgpvAMrules.R
+++ R/sgpvAMrules.R
@@ -6,63 +6,45 @@
 #'
 #'
 sgpvAMrules <- function(mcmcMonitoring, fork=TRUE, socket = TRUE, cores = detectCores(),
-                        waitWidth,
-                        sdY,
-                        waitEmpirical,
-                        minWaitN,
-                        midPointLess,
-                        midPointGreater,
+                        waitTime,
                         lookSteps,
                         kSteps,
                         maxAlertSteps,
                         monitoringIntervalLevel,
-                        maxN, lagOutcomeN){
+                        getUnrestricted, maxN, lagOutcomeN){
+
 
   if(fork==TRUE){
     # Only works on POSIX systems (Mac, Linux, Unix, BSD) and not Windows.
     mcmcEOS <- parallel::mclapply(mcmcMonitoring, sgpvAMrulesSingle,
-                                  waitWidth                = waitWidth,
-                                  sdY                      = sdY,
-                                  waitEmpirical            = waitEmpirical,
-                                  minWaitN                 = minWaitN,
-                                  midPointLess             = midPointLess,
-                                  midPointGreater          = midPointGreater,
+                                  waitTime                 = waitTime,
                                   lookSteps                = lookSteps,
                                   kSteps                   = kSteps,
                                   maxAlertSteps            = maxAlertSteps,
                                   monitoringIntervalLevel  = monitoringIntervalLevel,
-                                  maxN = maxN, lagOutcomeN = lagOutcomeN , mc.cores = cores)
+                                  getUnrestricted = getUnrestricted, maxN = maxN, lagOutcomeN = lagOutcomeN,
+                                  mc.cores = cores)
 
   } else if(socket==TRUE){
     # Works on Mac and Windows; only slightly slower
     cl             <- parallel::makeCluster(cores)
     on.exit(parallel::stopCluster(cl), add = TRUE)
     mcmcEOS <- parallel::parLapply(cl, mcmcMonitoring, sgpvAMrulesSingle,
-                                   waitWidth                = waitWidth,
-                                   sdY                      = sdY,
-                                   waitEmpirical            = waitEmpirical,
-                                   minWaitN                 = minWaitN,
-                                   midPointLess             = midPointLess,
-                                   midPointGreater          = midPointGreater,
+                                   waitTime                 = waitTime,
                                    lookSteps                = lookSteps,
                                    kSteps                   = kSteps,
                                    maxAlertSteps            = maxAlertSteps,
                                    monitoringIntervalLevel  = monitoringIntervalLevel,
-                                   maxN = maxN, lagOutcomeN = lagOutcomeN)
+                                   getUnrestricted = getUnrestricted, maxN = maxN, lagOutcomeN = lagOutcomeN)
   } else {
 
     mcmcEOS <- lapply(mcmcMonitoring, sgpvAMrulesSingle,
-                      waitWidth                = waitWidth,
-                      sdY                      = sdY,
-                      waitEmpirical            = waitEmpirical,
-                      minWaitN                 = minWaitN,
-                      midPointLess             = midPointLess,
-                      midPointGreater          = midPointGreater,
+                      waitTime                 = waitTime,
                       lookSteps                = lookSteps,
                       kSteps                   = kSteps,
                       maxAlertSteps            = maxAlertSteps,
                       monitoringIntervalLevel  = monitoringIntervalLevel,
-                      maxN = maxN, lagOutcomeN = lagOutcomeN)
+                      getUnrestricted = getUnrestricted, maxN = maxN, lagOutcomeN = lagOutcomeN)
   }
 
   # Return End of Study
