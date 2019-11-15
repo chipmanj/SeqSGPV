b25ad7634f569115942fec5778c0b3c862ea4af7
Jonathan Chipman <jonathanchipman@jonathans-mini.hci.utah.edu>
2019-11-15 16:03:39 -0700

    Function to get list all inputs of a given function, including default inputs

---
+++ R/match.call.defaults.R
@@ -0,0 +1,13 @@
+# Return inputs of call, including default inputs
+# https://stackoverflow.com/questions/14397364/match-call-with-default-arguments
+# Answer from Neal Fultz
+match.call.defaults <- function(...) {
+  call <- evalq(match.call(expand.dots = FALSE), parent.frame(1))
+  formals <- evalq(formals(), parent.frame(1))
+
+  for(i in setdiff(names(formals), names(call)))
+    call[i] <- list( formals[[i]] )
+
+
+  match.call(sys.function(sys.parent()), call)
+}
