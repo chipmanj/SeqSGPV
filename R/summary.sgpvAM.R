ad8740740ac1400cba02686bc5e78aefafb39b10
Jonathan Chipman <jonathanchipman@jonathans-mini.hci.utah.edu>
2019-11-15 16:11:16 -0700

    change from wait width to wait time, only summarize results unrestricted, lagged, and maxN results if obtained.

--- R/summary.sgpvAM.R
+++ R/summary.sgpvAM.R
@@ -2,6 +2,8 @@
 summary.sgpvAM <- function(am, alertK, waitTime, treatEffect, rd = 4){
 
 
+  # Inputs stored in am object
+  amInputs <- am$inputs
 
 
   # Select treatment effect to summarize and reduce am object
@@ -22,17 +24,19 @@ summary.sgpvAM <- function(am, alertK, waitTime, treatEffect, rd = 4){
 
   } else if(!is.function(am$inputs$effectGeneration)) {
 
-    te  <- am$inputs$effectGeneration
+    te  <- amInputs$effectGeneration
     o   <- am
-  } else {return(NULL)}
+  } else {
+    return(NULL)
+  }
 
-    lag  <- o$inputs$lagOutcomeN
-    maxN <- o$inputs$maxN
+  lag  <- amInputs$lagOutcomeN
+  maxN <- amInputs$maxN
 
 
   # Select and reduce to wait time to summarize
   if(missing(waitTime)){
-    cat(paste0(paste0("select wait time until confidence interval width of:\n",
+    cat(paste0(paste0("select wait time until applying monitoring rules:\n",
                       paste0(unname(sapply(names(o[["mcmcEndOfStudy"]]),
                                            FUN = function(x) as.numeric(strsplit(x,split="_")[[1]][2]))),
                              collapse=", "))))
@@ -45,11 +49,11 @@ summary.sgpvAM <- function(am, alertK, waitTime, treatEffect, rd = 4){
 
   # Select and reduce to number of affirmation steps required for stopping
   if(missing(alertK)){
-    cat(paste0(paste0("select wait time until confidence interval width of:\n",
+    cat(paste0(paste0("select required affirmation steps:\n",
                       paste0(o[,"alertK"], collapse=", "))))
     k <- readline(prompt="input: ")
 
-  } else k <- 50
+  } else k <- alertK
 
   o <- o[o[,"alertK"]==k,]
 
@@ -57,51 +61,53 @@ summary.sgpvAM <- function(am, alertK, waitTime, treatEffect, rd = 4){
 
 
 
-  cat(paste0("Given: theta = ", te, ", wait CI width = ", w, ", and k = ",k))
+  cat(paste0("Given: theta = ", te, ", wait time = ", w, ", and k = ",k))
 
-  cat("\n\nUnrestricted Sample Size with immediate outcomes:")
-  cat(paste0("\nAverage Observed Sample Size       = ", round(o["n"],rd)))
-  cat(paste0("\nP( reject point null )             = ", o["rejPN"]))
-  cat(paste0("\nP( conclude not trivial effect )   = ", o["stopNotTrivial"]))
-  cat(paste0("\nP( conclude not impactful effect ) = ", o["stopNotImpactful"]))
-  cat(paste0("\nP( conclude inconclusive )         = ", o["stopInconclusive"]))
-  cat(paste0("\nCoverage                           = ", round(o["cover"],rd)))
-  cat(paste0("\nBias                               = ", round(o["bias"],rd)))
-
-
-  if(length((grep("lag.",names(o))))>0){
-    cat(paste0("\n\nUnrestricted Sample Size, stopping and then observing the ", lag, " lagged outcomes:"))
-    cat(paste0("\nAverage Total Sample Size          = ", round(o["lag.n"],rd)))
-    cat(paste0("\nP( reject point null )             = ", o["lag.rejPN"]))
-    cat(paste0("\nP( conclude not trivial effect )   = ", o["lag.stopNotTrivial"]))
-    cat(paste0("\nP( conclude not impactful effect ) = ", o["lag.stopNotImpactful"]))
-    cat(paste0("\nP( conclude inconclusive )         = ", o["lag.stopInconclusive"]))
-    cat(paste0("\nCoverage                           = ", round(o["lag.cover"],rd)))
-    cat(paste0("\nBias                               = ", round(o["lag.bias"],rd)))
+  if(is.element("n",names(o))){
+    cat("\n\nUnrestricted sample size (immediate outcomes):")
+    cat(paste0("\nAverage sample size           = ", round(o["n"],rd)))
+    cat(paste0("\nP( reject point null )        = ", o["rejPN"]))
+    cat(paste0("\nP( conclude not ROPE effect ) = ", o["stopNotROPE"]))
+    cat(paste0("\nP( conclude not ROME effect ) = ", o["stopNotROME"]))
+    cat(paste0("\nP( conclude inconclusive )    = ", o["stopInconclusive"]))
+    cat(paste0("\nCoverage                      = ", round(o["cover"],rd)))
+    cat(paste0("\nBias                          = ", round(o["bias"],rd)))
   }
 
 
-  if(length((grep("maxN.",names(o))))>0){
-    cat(paste0("\n\nMaximum Observed Sample Size of: ",maxN))
-    cat(paste0("\nAverage Observed Sample Size       = ", round(o["maxN.n"],rd)))
-    cat(paste0("\nP( reject point null )             = ", o["maxN.rejPN"]))
-    cat(paste0("\nP( conclude not trivial effect )   = ", o["maxN.stopNotTrivial"]))
-    cat(paste0("\nP( conclude not impactful effect ) = ", o["maxN.stopNotImpactful"]))
-    cat(paste0("\nP( conclude inconclusive )         = ", o["maxN.stopInconclusive"]))
-    cat(paste0("\nCoverage                           = ", round(o["maxN.cover"],rd)))
-    cat(paste0("\nBias                               = ", round(o["maxN.bias"],rd)))
+  if(is.element("lag.n",names(o))){
+    cat(paste0("\n\nUnrestricted sample size (stopping and then observing ", lag, " lagged outcomes):"))
+    cat(paste0("\nAverage Sample Size           = ", round(o["lag.n"],rd)))
+    cat(paste0("\nP( reject point null )        = ", o["lag.rejPN"]))
+    cat(paste0("\nP( conclude not ROPE effect ) = ", o["lag.stopNotROPE"]))
+    cat(paste0("\nP( conclude not ROME effect ) = ", o["lag.stopNotROME"]))
+    cat(paste0("\nP( conclude inconclusive )    = ", o["lag.stopInconclusive"]))
+    cat(paste0("\nCoverage                      = ", round(o["lag.cover"],rd)))
+    cat(paste0("\nBias                          = ", round(o["lag.bias"],rd)))
   }
 
 
-  if(length((grep("lagMaxN.",names(o))))>0){
-    cat(paste0("\n\nMaximum Observed Sample Size of ",maxN,", and then observing the", lag, " lagged outcomes:"))
-    cat(paste0("\nAverage Total Sample Size          = ", round(o["lagMaxN.n"],rd)))
-    cat(paste0("\nP( reject point null )             = ", o["lagMaxN.rejPN"]))
-    cat(paste0("\nP( conclude not trivial effect )   = ", o["lagMaxN.stopNotTrivial"]))
-    cat(paste0("\nP( conclude not impactful effect ) = ", o["lagMaxN.stopNotImpactful"]))
-    cat(paste0("\nP( conclude inconclusive )         = ", o["lagMaxN.stopInconclusive"]))
-    cat(paste0("\nCoverage                           = ", round(o["lagMaxN.cover"],rd)))
-    cat(paste0("\nBias                               = ", round(o["lagMaxN.bias"],rd)))
+  if(is.element("maxN.n",names(o))){
+    cat(paste0("\n\nMaximum sample size of ",maxN," (immediate outcomes):"))
+    cat(paste0("\nAverage Observed Sample Size  = ", round(o["maxN.n"],rd)))
+    cat(paste0("\nP( reject point null )        = ", o["maxN.rejPN"]))
+    cat(paste0("\nP( conclude not ROPE effect)  = ", o["maxN.stopNotROPE"]))
+    cat(paste0("\nP( conclude not ROME effect ) = ", o["maxN.stopNotROME"]))
+    cat(paste0("\nP( conclude inconclusive )    = ", o["maxN.stopInconclusive"]))
+    cat(paste0("\nCoverage                      = ", round(o["maxN.cover"],rd)))
+    cat(paste0("\nBias                          = ", round(o["maxN.bias"],rd)))
+  }
+
+
+  if(is.element("lagMaxN.n",names(o))){
+    cat(paste0("\n\nMaximum sample size of ",maxN," (stopping and then observing ", lag, " lagged outcomes):"))
+    cat(paste0("\nAverage Total Sample Size     = ", round(o["lagMaxN.n"],rd)))
+    cat(paste0("\nP( reject point null )        = ", o["lagMaxN.rejPN"]))
+    cat(paste0("\nP( conclude not ROPE effect ) = ", o["lagMaxN.stopNotROPE"]))
+    cat(paste0("\nP( conclude not ROME effect ) = ", o["lagMaxN.stopNotROME"]))
+    cat(paste0("\nP( conclude inconclusive )    = ", o["lagMaxN.stopInconclusive"]))
+    cat(paste0("\nCoverage                      = ", round(o["lagMaxN.cover"],rd)))
+    cat(paste0("\nBias                          = ", round(o["lagMaxN.bias"],rd)))
   }
 
 }
