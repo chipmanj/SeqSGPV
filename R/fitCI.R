96c4dffe499620e75318cf5caac5d68bbe7610d0
Jonathan Chipman <jonathanchipman@Jonathans-Mac-mini.local>
2020-04-01 16:43:41 -0600

    1. input X matrix for lmCI and lrCI 2. use fastglmPure for lrCI

--- R/fitCI.R
+++ R/fitCI.R
@@ -4,7 +4,7 @@
 ##' @title Functions for obtaining Confidence Intervals under a linear model and under logistic regression
 ##'
 ##' @param y outcomes
-##' @param trt treatment assignments for each patient
+##' @param X X matrix with last column corresponding to coefficient of interest
 ##' @param look current number of patients accrued
 ##' @param miLevel alpha in traditional (1-alpha) confidence interval used in monitoring intervals
 ##'
@@ -12,9 +12,9 @@
 
 ##' @rdname fitCI
 ##' @export
-lmCI <- function(y, trt, look, miLevel){
+lmCI <- function(y, X, look, miLevel){
   # ols CIs
-  f <- fastLmPure(X = as.matrix(cbind(1,trt[1:look])), y = y[1:look])
+  f <- RcppEigen::fastLmPure(X = as.matrix(X[1:look,]), y = y[1:look])
   eci <- c(f$coefficients[2],
            f$coefficients[2] + c(-1,1) * qt(1-miLevel/2, df = f$df.residual) * f$se[2])
   return(eci)
@@ -26,14 +26,13 @@ class(lmCI) <- "normal"
 ##' @export
 lrCI <- function(y, trt, look, miLevel){
   # logistic regression CIs
-  f     <- glm(y[1:look] ~ trt[1:look], family = binomial)
-  coefs <- summary(f)$coefficients
-  est   <- exp( coefs[2,"Estimate"] )
-  eci   <- c(est,exp( coefs[2,"Estimate"] + c(-1,1) * qnorm(1-miLevel/2) * coefs[2,"Std. Error"] ))
-  # Infinite CI bounds may occur with binomial data and insufficient
+  f <- fastglm::fastglmPure(x = as.matrix(X[1:look,]), y=y[1:look],family = binomial())
+  eci <- exp(c(f$coefficients[2],
+               f$coefficients[2] + c(-1,1) * qnorm(1-miLevel/2) * f$se[2]))
+  # Infinite upper CI bound may occur with binomial data and insufficient
   #  data to estimate an effect and error.  Set infinite bounds to 10^10
   eci[is.infinite(eci)] <- 10^10
-  return(ci)
+  return(eci)
 
 }
 class(lrCI) <- "binomial"
