fcf46017f85af3340574204042952a49e83bb797
Jonathan Chipman <jonathanchipman@Jonathans-Mac-mini.local>
2020-06-29 09:41:15 -0700

    changed from rejPN to rejH0 to account for one-sided hypotheses

--- R/addStats.R
+++ R/addStats.R
@@ -10,7 +10,7 @@
 #
 # Inputs
 # o        : matrix with at least columns: theta, est, lo (lower CI bound), and up (upper CI bound)
-# pointNull: specified point null
+# effectPN : specified effect point null
 # deltaL2  : lower delta furthest from point null
 # deltaL1  : lower delta closest    to point null
 # deltaG1  : upper delta furthest from point null
@@ -26,31 +26,50 @@
 
 
 
-addStats <- function(o, pointNull, deltaL2, deltaL1, deltaG1, deltaG2){
+addStats <- function(o, effectPN, deltaL2, deltaL1, deltaG1, deltaG2){
 
-  # Add whether reject point null, coverage, and bias
-  rejPN <- as.numeric(o[,"lo"] < pointNull & o[,"up"] < pointNull |
-                      o[,"lo"] > pointNull & o[,"up"] > pointNull)
-  cover <- as.numeric(o[,"lo"] < o[,"theta"] & o[,"theta"] < o[,"up"])
-  bias  <- o[,"est"] - o[,"theta"]
+  # Add whether coverage and bias
+  cover <- as.numeric(o[,"lo"] < o[,"effect1"] & o[,"effect1"] < o[,"up"])
+  bias  <- o[,"est"] - o[,"effect1"]
 
 
-  # Obtain sgpv
+  # Add whether reject H0 and obtain sgpv
   if(!anyNA(c(deltaL2, deltaL1, deltaG1, deltaG2))){
-
     # Two sided
+
+    rejH0   <- as.numeric(o[,"lo"] < effectPN & o[,"up"] < effectPN |
+                          o[,"lo"] > effectPN & o[,"up"] > effectPN)
+
     sgpvROPE  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaL1, null.hi = deltaG1)$p.delta
     sgpvROME  <- 1 - sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaL2, null.hi = deltaG2)$p.delta
+
   } else if(!anyNA(c(deltaL2, deltaL1))){
-
     # One sided: efficacy when less than null
-    sgpvROPE  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =  deltaL1, null.hi =   10^10)$p.delta
-    sgpvROME  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =   -10^10, null.hi = deltaL2)$p.delta
-  } else if(!anyNA(c(deltaG1, deltaG2))){
 
+    rejH0 <- as.numeric(o[,"lo"] < effectPN & o[,"up"] < effectPN)
+
+    # suppress warning that at least one interval has inviting length
+    defaultW <- getOption("warn")
+    options(warn = -1)
+
+    sgpvROPE  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaL1, null.hi =     Inf)$p.delta
+    sgpvROME  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =    -Inf, null.hi = deltaL2)$p.delta
+
+    options(warn = defaultW)
+
+  } else if(!anyNA(c(deltaG1, deltaG2))){
     # One sided: efficacy when greater than null
-    sgpvROPE  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =  -10^10, null.hi = deltaG1)$p.delta
-    sgpvROME  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaG2, null.hi =   10^10)$p.delta
+
+    rejH0 <- as.numeric(o[,"lo"] > effectPN & o[,"up"] > effectPN)
+
+    # suppress warning that at least one interval has inviting length
+    defaultW <- getOption("warn")
+    options(warn = -1)
+
+    sgpvROPE  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo =    -Inf, null.hi = deltaG1)$p.delta
+    sgpvROME  <-     sgpv::sgpvalue(est.lo = o[,"lo"], est.hi = o[,"up"], null.lo = deltaG2, null.hi =     Inf)$p.delta
+
+    options(warn = defaultW)
   } else{
 
     stop("A one sided study requires both deltas to be strictly greater or lower than point null")
@@ -58,7 +77,7 @@ addStats <- function(o, pointNull, deltaL2, deltaL1, deltaG1, deltaG2){
 
 
   # Return appended statistics
-  cbind(o, bias, rejPN, cover, sgpvROPE, sgpvROME)
+  cbind(o, bias, rejH0, cover, sgpvROPE, sgpvROME)
 
 }
 
