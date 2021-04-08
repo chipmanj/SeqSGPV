0343b03d61b837b2121b91ec088c341a5a19e63a
Jonathan Chipman <jonathanchipman@Jonathans-Mac-mini.local>
2021-04-08 13:36:14 -0600

    Require modelFit function to have lm (OLS) for locationShift

--- R/locationShift.R
+++ R/locationShift.R
@@ -2,9 +2,8 @@
 locationShift <- function(o, shiftedThetas, printProgress=TRUE){
 
   # Only perform given data generated under fixed effect and of location-shift family
-  if(  is.function(o$inputs$effectGeneration) |
-      !class(o$inputs$modelFit) %in% c("normal","uniform","t")){
-    stop("Location shift only performed for data generated under fixed treatment effect and location-shift family.")
+  if(  is.function(o$inputs$effectGeneration) | any(grepl("Lm|lm", deparse(lagsROE$inputs$modelFit)))  ){
+    stop("Currently, location shift only performed for data generated under fixed treatment effect and using an OLS model.")
   }
 
 
