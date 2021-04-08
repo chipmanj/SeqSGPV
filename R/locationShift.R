ba3d4b9de5acd6ce1ecedb257c811e1aac3a2446
Jonathan Chipman <jonathanchipman@Jonathans-Mac-mini.local>
2021-04-08 14:54:02 -0600

    Updated for compatability with sgpvAM: shifting "effect1" rather than "theta".

--- R/locationShift.R
+++ R/locationShift.R
@@ -1,8 +1,8 @@
 #' @export
-locationShift <- function(o, shiftedThetas, printProgress=TRUE){
+locationShift <- function(o, shiftedEffects, printProgress=TRUE){
 
   # Only perform given data generated under fixed effect and of location-shift family
-  if(  is.function(o$inputs$effectGeneration) | any(grepl("Lm|lm", deparse(o$inputs$modelFit)))  ){
+  if(  is.function(o$inputs$effectGeneration) | !any(grepl("Lm|lm", deparse(o$inputs$modelFit)))  ){
     stop("Currently, location shift only performed for data generated under fixed treatment effect and using an OLS model.")
   }
 
@@ -10,16 +10,16 @@ locationShift <- function(o, shiftedThetas, printProgress=TRUE){
   # Keep only columns:
   #  theta, n, y, trt, est, lo, up
   keepColumns <- function(simData){
-    simData <- simData[,c("theta", "n", "y", "trt", "est", "lo", "up")]
+    simData <- simData[,c("theta0", "effect1","n", "y", "trt", "est", "lo", "up")]
     simData
   }
   mcmcMonitoring <- lapply(X=o[["mcmcMonitoring"]], keepColumns)
 
   # Function to location shift treatment effect
-  shiftTheta <- function(simData, shift){
+  shiftEffect <- function(simData, shift){
 
-    simData[simData[,"trt"]==1,"y"]      <- simData[simData[,"trt"]==1,"y"]      + shift
-    simData[,c("theta","est","lo","up")] <- simData[,c("theta","est","lo","up")] + shift
+    simData[simData[,"trt"]==1,"y"]        <- simData[simData[,"trt"]==1,"y"]        + shift
+    simData[,c("effect1","est","lo","up")] <- simData[,c("effect1","est","lo","up")] + shift
     simData
 
   }
@@ -28,37 +28,37 @@ locationShift <- function(o, shiftedThetas, printProgress=TRUE){
   # Store results for each location shifted data
   mcmcEndOfStudyShifted <- list()
 
-  # Make note of original value of theta
+  # Make note of original value of effect
   effectOriginal <- o$inputs$effectGeneration
 
 
-  for(shift in shiftedThetas){
+  for(shift in shiftedEffects){
     if(printProgress) print(paste0("theta shifted by: ",shift))
 
 
     # Use previously generated study design if provided in object o
     if(shift==0){
-      mcmcEndOfStudyShifted[[paste0("theta_",effectOriginal)]]                     <- o
-      mcmcEndOfStudyShifted[[paste0("theta_",effectOriginal)]][["mcmcMonitoring"]] <- NULL
-      mcmcEndOfStudyShifted[[paste0("theta_",effectOriginal)]][["inputs"]][["mcmcData"]] <- NULL
+      mcmcEndOfStudyShifted[[paste0("effect_",effectOriginal)]]                           <- o
+      mcmcEndOfStudyShifted[[paste0("effect_",effectOriginal)]][["mcmcMonitoring"]]       <- NULL
+      mcmcEndOfStudyShifted[[paste0("effect_",effectOriginal)]][["inputs"]][["mcmcData"]] <- NULL
       next
     }
 
     # New shifted study design
     oo                         <- o
-    oo$inputs$mcmcData         <- lapply(X = mcmcMonitoring, shiftTheta, shift = shift)
+    oo$inputs$mcmcData         <- lapply(X = mcmcMonitoring, shiftEffect, shift = shift)
     oo$inputs$effectGeneration <- effectOriginal + shift
 
-    mcmcEndOfStudyShifted[[paste0("theta_",oo$inputs$effectGeneration)]] <- do.call(sgpvAM, args=oo$inputs)
+    mcmcEndOfStudyShifted[[paste0("effect_",oo$inputs$effectGeneration)]] <- do.call(sgpvAM, args=oo$inputs)
 
     # If additional data was required for monitoring, keep for future use
-    mcmcMonitoringNew <- mcmcEndOfStudyShifted[[paste0("theta_",oo$inputs$effectGeneration)]]$mcmcMonitoring
-    mcmcShiftBack     <- lapply(X = mcmcMonitoringNew, shiftTheta, shift = -shift)
+    mcmcMonitoringNew <- mcmcEndOfStudyShifted[[paste0("effect_",oo$inputs$effectGeneration)]]$mcmcMonitoring
+    mcmcShiftBack     <- lapply(X = mcmcMonitoringNew, shiftEffect, shift = -shift)
     mcmcMonitoring    <- lapply(X = mcmcShiftBack, keepColumns)
 
     # Don't keep generated data in output
-    mcmcEndOfStudyShifted[[paste0("theta_",oo$inputs$effectGeneration)]]$mcmcMonitoring  <- NULL
-    mcmcEndOfStudyShifted[[paste0("theta_",oo$inputs$effectGeneration)]]$inputs$mcmcData <- NULL
+    mcmcEndOfStudyShifted[[paste0("effect_",oo$inputs$effectGeneration)]]$mcmcMonitoring  <- NULL
+    mcmcEndOfStudyShifted[[paste0("effect_",oo$inputs$effectGeneration)]]$inputs$mcmcData <- NULL
 
   }
 
